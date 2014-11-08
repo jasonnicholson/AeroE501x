4f3e2a190174a85dd35f376a1665b63e1bdb18b9
Jason Nicholson <jashale@yahoo.com>
2014-11-08 16:21:50 -0600

    Updated besselzero.m with new initial guess values for bessel function of the first and second kind.
    Signed-off-by: Jason Nicholson <jashale@yahoo.com>

--- besselzero/besselzero.m
+++ besselzero/besselzero.m
@@ -1,5 +1,5 @@
 function x = besselzero(n, k, kind)
-% besselzero calculates the zeros of Bessel function of the first and second kind 
+% besselzero calculates the zeros of Bessel function of the first and second kind
 %
 %   x = besselzero(n)
 %   x = besselzero(n, k)
@@ -13,7 +13,7 @@ function x = besselzero(n, k, kind)
 %
 %% Outputs
 % * x - The calculated zeros.  x has length of k.
-% Find first k positive zeros of the Bessel function J(n,x) or Y(n,x) 
+% Find first k positive zeros of the Bessel function J(n,x) or Y(n,x)
 % using Halley's method.
 %
 %% Description
@@ -29,84 +29,113 @@ function x = besselzero(n, k, kind)
 %   y = besselj(n, x);
 %   plot(x, y, z, zeros(size(z)),'x')
 
-% Originally written by 
+% Originally written by
 % Written by: Greg von Winckel - 01/25/05
 % Contact: gregvw(at)chtm(dot)unm(dot)edu
 %
-% Modified and Documented by 
+% Modified and Documented by
 % Jason Nicholson 2014-Nov-06
 % Contact: jashale@yahoo.com
 
 %% Change Log
 % * Original release. 2005-Jan-25, Greg von Winckel.
 % * Updated Documentation and commented algorithm. Fixed bug in finding the
-%   the first zero of the bessel function of the second kind. 2014-Nov-06, 
+%   the first zero of the bessel function of the second kind. 2014-Nov-06,
 %   Jason Nicholson.
 %
 
+x=nan(k,1);
 
-k3=3*k;
-
-x=zeros(k3,1);
-
-for j=1:k3
-    
-    % Initial guess of zeros 
-    x0=1+sqrt(2)+(j-1)*pi+n+n^0.4;
+for iRoot=1:k
+    % Initial guess of zeros
+    switch kind
+        case 1
+            switch iRoot
+                case 1
+                    c = [2.41383745006265;4.60333884879062;-0.00879177153608845];
+                    e = -1.97630722000738;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                case 2
+                    c = [5.63229987996047;4.60333884784611;-0.116235793367165];
+                    e = -0.955326192274869;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                case 3
+                    c = [8.85083146233771;4.60333884233918;-0.223115212659848];
+                    e = -0.872441208450964;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                otherwise
+                    x0 = x(iRoot-1) + x(3)-x(2);
+            end
+        case 2
+            switch iRoot
+                case 1
+                    c = [0.804622204593045;4.60333884793812;0.0810993745488260];
+                    e = -1.31499999976479;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                case 2
+                    c = [4.02305932678643;4.60333884907597;-0.0641172320834957];
+                    e = -1.03379588489646;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                case 3
+                    c = [7.24155745194042;4.60333884552478;-0.168875347977873];
+                    e = -0.907056015534608;
+                    x0 = c(1) + c(2)*n+c(3)*(n+1).^(e);
+                otherwise
+                    x0 = x(iRoot-1) + x(3)-x(2);
+            end
+        otherwise
+            error('Code should never get here.');
+            
+    end
     
     % Do Halley's method
-    x(j)=findzero(n,x0,kind);
-
-    if x(j)==inf
-        error('Bad guess.');
-    end
+    x(iRoot)=findzero(n,iRoot,x0,kind);
     
 end
 
-x=sort(x);
-dx=[1;abs(diff(x))];
-x=x(dx>1e-8);
+end
 
-x=x(1:k);
 
-function x=findzero(n,x0,kind)
 
-n1=n+1;     n2=n*n;
+function x=findzero(n,k,x0,kind)
+% Uses Halley's method to find a zero given the starting point x0
+% http://en.wikipedia.org/wiki/Halley's_method
 
-% Tolerance
-tol=1e-12;
+ITERATIONS_MAX = 100;       % Maximum number of iteration
+TOLERANCE_RELATIVE = 1e4;   % 16-4 = 11 significant digits
 
-% Maximum number of times to iterate
-MAXIT=100;
+% Setup loop
+error = 1;
+loopCount = 0;
+x = 1; % Initialization value only.  It is is not used.
 
-% Initial error
-err=1;
-
-iter=0;
-
-while abs(err)>tol & iter<MAXIT
+% Begin loop
+while abs(error)>eps(x)*TOLERANCE_RELATIVE && loopCount<ITERATIONS_MAX
     
     switch kind
         case 1
-            a=besselj(n,x0);    
-            b=besselj(n1,x0);   
+            a = besselj(n,x0);
+            b = besselj((n+1),x0);
         case 2
-            a=bessely(n,x0);
-            b=bessely(n1,x0);
+            a = bessely(n,x0);
+            b = bessely((n+1),x0);
     end
-            
-    x02=x0*x0;
     
-    err=2*a*x0*(n*a-b*x0)/(2*b*b*x02-a*b*x0*(4*n+1)+(n*n1+x02)*a*a);
+    xSquared = x0*x0;
     
-    x=x0-err;
+    error = 2*a*x0*(n*a-b*x0)/(2*b*b*xSquared-a*b*x0*(4*n+1)+(n*(n+1)+xSquared)*a*a);
+    
+    % Prepare for next loop
+    x=x0-error;
     x0=x;
-    iter=iter+1;
+    loopCount=loopCount+1;
     
 end
 
-if iter>MAXIT-1
-    warning('Failed to converge to within tolerance. ',...
-            'Try a different initial guess');
-    x=inf;    
+% Handle maximum iterations
+if loopCount>ITERATIONS_MAX-1
+    warning('Failed to converge to within relative tolerance of %e for n=%f and k=%d in %d iterations', eps(x)*TOLERANCE_RELATIVE, n, k, ITERATIONS_MAX);
+    x=NaN;
+end
+
 end
